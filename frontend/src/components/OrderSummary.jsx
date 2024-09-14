197d96cb210597d566d230bd15e946136eb43b07
Asad Ali <masadbali190@gmail.com>
2024-09-14 15:26:40 -0400

    Update OrderSummary.jsx

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51OC5ypApI0dkNpnnejivmV8jZ55sIue4ulBDv9S1zx5c4iMR3KpoNM5WpH1JX5RXaUaaOjo3z9uthC2kDmSeoCV500bMcKxStR"
 );
 
 const OrderSummary = () => {
